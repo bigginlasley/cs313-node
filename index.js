93be091bce53a5bfff6fd1ebd4413e7439526812
Biggin <bigginlasley@gmail.com>
2019-03-16 21:38:06 -0600

    fixing error

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config();
 const PORT = process.env.PORT || 5000
 const app = express();
-const connectionString = process.env.DATABASE_URL || "postgres://rsoimcscknisfn:a615490690f6ed303425a81976b9b6e98e73337cbd453a0edbc268de75cdc48c@ec2-75-101-133-29.compute-1.amazonaws.com:5432/d1ih6clrpjaqkd?ssl=true";
-const pool = new Pool({connectionString: connectionString});
+const connectionString = process.env.DATABASE_URL;
+//const pool = new Pool({connectionString: connectionString});
 
 const controller = require('./funct.js');
 
+const app = express();
 
-
-
-
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('public/index'))
-  .get('/postage', controller.handler)//will need to change this
+  .get('/postage', handler)//will need to change this
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  
+  function getActivites(request, response){
+    const aType = request.query.type;
+  }
 
-
-
+  // function handler(request, response){
+  //   const weight = request.query.weight;
+  //   const p_type = request.query.postage_type;
