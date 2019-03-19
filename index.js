8e06b8e36b346183e32fe45578bbbd3853de062a
Biggin <bigginlasley@gmail.com>
2019-03-18 19:22:02 -0600

    still working on fixing an error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000
 const app = express();
 const connectionString = process.env.DATABASE_URL;
-//const pool = new Pool({connectionString: connectionString});
+const pool = new Pool({connectionString: connectionString});
 
 const controller = require('./funct.js');
 
@@ -13,164 +13,26 @@ app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('public/index'))
-  .get('/postage', handler)//will need to change this
+  .get('/filter', getActivites)//will need to change this
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   function getActivites(request, response){
     const aType = request.query.type;
+
+    getTypefromDB(type, function(error, result) {
+
+      if (error || result == null) {
+        response.status(500).json({success: false, data: error});
+      } else {
+        const type = result[0];
+        response.status(200).json(type);
+      }
+    });
   }
 
-  // var price;
+  function getTypefromDB(type, callback){
+    const sql = 'SELECT id FROM activity_type WHERE name= ${type}';
 
-  // function handler(request, response){
-  //   const weight = request.query.weight;
-  //   const p_type = request.query.postage_type;
+  }
 
-  //   if(weight > 3.5)
-  //     first(response, weight, p_type);
-
-  //   switch(p_type){
-  //     case "Stamped Letter":
-  //       stamped(response, weight, p_type);
-  //       break;
-  //     case "Metered Letter":
-  //       metered(response, weight, p_type);
-  //       break;
-  //     case "Large Envelope":
-  //       large(response, weight, p_type);
-  //       break;
-  //     case "First-Class Package":
-  //       first(response, weight, p_type);
-  //       break;
-  //   }
-  // }
-
-  // function stamped(response, weight, p_type){
-  //   if(weight <= 1)
-  //     price = 0.55;
-  //   else if(weight <= 2)
-  //     price = Number.parseFloat(0.70).toFixed(2);
-  //   else if(weight <= 3)
-  //     price = 0.85;
-  //   else
-  //     price = Number.parseFloat(1.00).toFixed(2);
-
-  //   send(response, weight, p_type, price);
-  // }
-
-  // function metered(response, weight, p_type){
-
-  //   if(weight <= 1)
-  //     price = Number.parseFloat(0.50).toFixed(2);
-  //   else if(weight <= 2)
-  //     price = 0.65;
-  //   else if(weight <= 3)
-  //     price = Number.parseFloat(0.80).toFixed(2);
-  //   else
-  //     price = 0.95;
-
-  //   send(response, weight, p_type, price);
-  // }
-  
-
-  // function large(response, weight, p_type){
-  //   weight = Math.ceil(weight);
-  //   switch(weight){
-  //     case 1:
-  //       price = Number.parseFloat(1.00).toFixed(2);
-  //       break;
-  //     case 2:
-  //       price = Number.parseFloat(1.15).toFixed(2); 
-  //       break;
-  //     case 3:
-  //       price = Number.parseFloat(1.30).toFixed(2);
-  //       break;
-  //     case 4:
-  //       price = Number.parseFloat(1.45).toFixed(2);
-  //       break;
-  //     case 5:
-  //       price = Number.parseFloat(1.60).toFixed(2);
-  //       break;
-  //     case 6:
-  //       price = Number.parseFloat(1.75).toFixed(2);
-  //       break;
-  //     case 7:
-  //       price = Number.parseFloat(1.90).toFixed(2);
-  //       break;
-  //     case 8:
-  //       price = Number.parseFloat(2.05).toFixed(2);
-  //       break;
-  //     case 9:
-  //       price = Number.parseFloat(2.20).toFixed(2);
-  //       break;
-  //     case 10:
-  //       price = Number.parseFloat(2.35).toFixed(2);
-  //       break;
-  //     case 11:
-  //       price = Number.parseFloat(2.50).toFixed(2);
-  //       break;
-  //     case 12:
-  //       price = Number.parseFloat(2.65).toFixed(2);
-  //       break;
-  //     case 13:
-  //       price = Number.parseFloat(2.80).toFixed(2);
-  //       break;
-  //   }
-  //   send(response, weight, p_type, price);
-  // }
-
-  // function first(response, weight, p_type){
-    
-  //   weight = Number(Math.ceil(weight));
-  //   switch(weight){
-  //     case 1:
-  //       price = 3.66;
-  //       break;
-  //     case 2:
-  //       price = 3.66;
-  //       break;
-  //     case 3:
-  //       price = 3.66;
-  //       break;
-  //     case 4:
-  //       price = 3.66;
-  //       break;
-  //     case 5:
-  //       price = 4.39;
-  //       break;
-  //     case 6:
-  //       price = 4.39;
-  //       break;
-  //     case 7:
-  //       price = 4.39;
-  //       break;
-  //     case 8:
-  //       price = 4.39;
-  //       break;
-  //     case 9:
-  //       price = 5.19;
-  //       break;
-  //     case 10:
-  //       price = 5.19;
-  //       break;
-  //     case 11:
-  //       price = 5.19;
-  //       break;
-  //     case 12:
-  //       price = 5.19;
-  //       break;
-  //     case 13:
-  //       price = 5.71;
-  //       break;
-  //   }
-  //   send(response, weight, p_type, price);
-  // }
-
-  // function send(response, w, p_t, p)
-  // {
-  //   const params = {weight: w, p_type: p_t, price: p};
-  //   response.render('pages/results', params);
-  // }
-
-
-
+ 
