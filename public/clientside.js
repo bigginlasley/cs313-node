2dec071867f20576a9c27663543ccc47e210281e
Biggin <bigginlasley@gmail.com>
2019-04-08 06:52:28 -0600

    added create event

--- public/clientside.js
+++ public/clientside.js
@@ -4,6 +4,15 @@ console.log("Filter: " + filter);
 $.get("/filter", {filter:filter}, function(data) {
     console.log("Back from the server with:");
     console.log(data);
+    $("#act_list").empty().append("<tr><th>Name</th><th>Type</th><th>location</th><th>time</th><th>Capacity</th><th>Count</th></tr>")
+
+    for (var i =0; i < data.list.length; i++){
+        var act = data.list[i];
+        $("#act_list").append("<tr><td>"+ act.name + "</td><td>"+ act.at_name + "</td><td>" + act.location + "</td><td>"+ act.time + "</td><td>" + act.capacity + "</td><td>" + act.count + "</td></tr>");
+        console.log(act);
+    }
+
+    
 })
 }
 
@@ -12,7 +21,22 @@ function getAll(){
     $.get("/activities", function(data) {
         console.log("Back from the server with:");
         console.log(data);
+
+
+        $("#act_list").empty().append("<tr><th>Name</th><th>Type</th><th>location</th><th>time</th><th>Capacity</th><th>Count</th></tr>")
+
+    for (var i =0; i < data.list.length; i++){
+        var act = data.list[i];
+        $("#act_list").append("<tr><td>"+ act.name + "</td><td>"+ act.at_name + "</td><td>" + act.location + "</td><td>"+ act.time + "</td><td>" + act.capacity + "</td><td>" + act.count + "</td></tr>");
+        console.log(act);
+    }
     })
+}
+
+
+
+function addOn(){
+    $.post("/createActivity")
 }
 //         <form action="/filter" method="GET">
 // <select id='atype' name='atype'>
